5682e8f965d555f98abe3f933d28600cc0dd6012
Abhiraj Aditya <abhiaditya861@gmail.com>
2023-07-12 14:25:10 +0530

    final

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePatterns = [];
 var userClickedPattern = [];
 var level = 0;
-
+var dummy = 0;
 
 $("body").keypress(function() {
     nextSequence();
